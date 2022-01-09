d0765826ac91aee355248f961a4d825f836f9aa1
KGolemo <58262773+KGolemo@users.noreply.github.com>
2022-01-09 20:49:07 +0100

    Add Procfile, runtime.txt

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from joblib import load
 
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
 server = app.server
 
 ######################################################################################################################
